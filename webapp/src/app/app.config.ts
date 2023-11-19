84f0c290b2d8a9f70f142464527034e02e0f1e77
genka <tamani.coding@gmail.com>
2023-11-19 07:05:28 +0100

    refactore webapp

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,8 +1,44 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { AuthConfig, OAuthService, provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideHttpClient } from '@angular/common/http';
+
+export const authCodeFlowConfig: AuthConfig = {
+  issuer: 'http://localhost:8180/realms/my-test-realm',
+  tokenEndpoint: 'http://localhost:8180/realms/my-test-realm/protocol/openid-connect/token',
+  redirectUri: window.location.origin,
+  clientId: 'my-webapp-client',
+  responseType: 'code',
+  scope: 'openid profile',
+  showDebugInformation: true,
+};
+
+function initializeOAuth(oauthService: OAuthService): Promise<void> {
+  return new Promise((resolve) => {
+    oauthService.configure(authCodeFlowConfig);
+    oauthService.setupAutomaticSilentRefresh();
+    oauthService.loadDiscoveryDocumentAndLogin()
+      .then(() => resolve());
+  });
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideOAuthClient(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (oauthService: OAuthService) => {
+        return () => {
+          initializeOAuth(oauthService);
+        }
+      },
+      multi: true,
+      deps: [
+        OAuthService
+      ]
+    }
+  ]
 };
--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,43 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { provideOAuthClient, AuthConfig, OAuthService } from 'angular-oauth2-oidc';
-import { APP_INITIALIZER } from '@angular/core';
+import { appConfig } from './app/app.config';
 
-export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'http://localhost:8180/realms/my-test-realm',
-  tokenEndpoint: 'http://localhost:8180/realms/my-test-realm/protocol/openid-connect/token',
-  redirectUri: window.location.origin,
-  clientId: 'my-webapp-client',
-  responseType: 'code',
-  scope: 'openid profile',
-  showDebugInformation: true,
-};
-
-function initializeOAuth(oauthService: OAuthService): Promise<void> {
-  return new Promise((resolve) => {
-    oauthService.configure(authCodeFlowConfig);
-    oauthService.setupAutomaticSilentRefresh();
-    oauthService.loadDiscoveryDocumentAndLogin()
-      .then(() => resolve());
-  });
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    provideOAuthClient(),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (oauthService: OAuthService) => {
-        return () => {
-          initializeOAuth(oauthService);
-        }
-      },
-      multi: true,
-      deps: [
-        OAuthService
-      ]
-    }
-  ],
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
